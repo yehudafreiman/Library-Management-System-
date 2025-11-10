ef7576fe558473650c5def0e4dd7eee71f2b5d13
Yehuda Freiman <yehfre@icloud.com>
2025-11-10 23:01:16 +0200

    Refactor menu prompts and add print statements

--- main.py
+++ main.py
@@ -5,44 +5,43 @@ from library import Library
 if __name__ == "__main__":
 
     my_library = Library()
+    print("Wellcome to The Lion Library!")
 
     is_running = True
     while is_running:
         print()
-        print("*** Wellcome to The Lion Library! ***\nChoose from the menu:")
+        print("menu: ")
         print(" 1.add book\n 2.add user\n 3.borrow book\n 4.return book\n 5.get available books\n 6.search book\n 7.for exit")
-        action = int(input("Enter here -> "))
+        action = int(input("Enter a number: "))
 
         if action == 1: # add book
             title = input("Enter book title: ")
             author = input("Enter book author: ")
             isbn = input("Enter book isbn: ")
-            my_library.add_book(Book(title, author, isbn))
-
+            print(my_library.add_book(Book(title, author, isbn)))
 
         if action == 2: # add user
             name = input("Enter user name: ")
             user_id = input("Enter user id: ")
-            my_library.add_user(User(name, user_id))
+            print(my_library.add_user(User(name, user_id)))
 
-        if action == 3: # borrow_book
-            user_id = input("Enter user id: ")
+        if action == 3: # borrow book
             book_isbn = input("Enter book isbn: ")
-            my_library.borrow_book(user_id, book_isbn)
+            user_id = input("Enter user id: ")
+            print(my_library.borrow_book(user_id, book_isbn))
 
-        if action == 4: # return_book
-            user_id = input("Enter user id: ")
+        if action == 4: # return book
             book_isbn = input("Enter book isbn: ")
-            my_library.return_book(user_id, book_isbn)
+            user_id = input("Enter user id: ")
+            print(my_library.return_book(user_id, book_isbn))
 
         if action == 5: # list available books
-            my_library.list_available_books()
+            print(my_library.list_available_books())
 
-        if action == 6: # search_book
+        if action == 6: # search book
             key = input("Enter some letters from title author to search: ")
-            my_library.search_book(key)
+            print(my_library.search_book(key))
 
         if action == 7: # exit
             print("Goodbye!")
             is_running = False
-
